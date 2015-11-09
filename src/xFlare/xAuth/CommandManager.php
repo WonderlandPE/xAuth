b14f4f2932575ea5922f88607e2469966a9ce217
xFlare <boughousp19@student.riverdell.org>
2015-11-09 18:59:22 -0500

    Command manager

---
+++ src/xFlare/xAuth/CommandManager.php
@@ -0,0 +1,25 @@
+<?php
+/*
+                            _     _     
+            /\             | |   | |    
+ __  __    /  \     _   _  | |_  | |__  
+ \ \/ /   / /\ \   | | | | | __| | '_ \ 
+  >  <   / ____ \  | |_| | | |_  | | | |
+ /_/\_\ /_/    \_\  \__,_|  \__| |_| |_|
+                                        
+                                        */
+namespace xFlare\xAuth;
+
+use pocketmine\event\Listener;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\Server;
+/*
+- Manages commands, xAuth commands are proccessed here.
+*/
+class CommandManager implements Listener{
+	public function __construct(Loader $plugin){
+        $this->plugin = $plugin;
+  }
+}
