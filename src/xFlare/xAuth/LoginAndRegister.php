973c38c4bf0df12d788263198db79d3abc911759
xFlare <boughousp19@student.riverdell.org>
2015-11-11 11:26:03 -0500

    Update LoginAndRegister.php

--- src/xFlare/xAuth/LoginAndRegister.php
+++ src/xFlare/xAuth/LoginAndRegister.php
@@ -9,7 +9,6 @@
                                         
                                         */
 
-#Controls logins and registers.
 namespace xFlare\xAuth;
 
 use pocketmine\event\Listener;
@@ -20,85 +19,38 @@ use pocketmine\utils\Config;
 use pocketmine\Server;
 use pocketmine\event\player\PlayerChatEvent;
 use pocketmine\event\player\PlayerJoinEvent;
-
 /*
-- 2 = Registered. (Implementing later...)
-- 1 = Logged in.
-- 0 = Not logged in or registered.
+- Logins/Registers players.
+- Main core functions are done here.
 */
-
 class LoginAndRegister implements Listener{
 	public function __construct(Loader $plugin){
         $this->plugin = $plugin;
     }
     public function onJoin(PlayerJoinEvent $event){
+    	//Messages
     	if($this->plugin->status === "enabled"){
     		$event->getPlayer()->sendMessage("[xAuth] This server is protected by xAuth.");
+    		$event->getPlayer()->sendMessage($this->plugin->getConfig()->get("join"));
     	}
-    	if($this->plugin->status === "enabled" and $this->plugin->provider === "yml"){
-            $myuser = new Config($this->plugin->getDataFolder() . "users/" . strtolower($event->getPlayer()->getName() . ".yml"), Config::YAML);
-        	if(!$this->plugin->registered->exists(strtolower($event->getPlayer()->getName()))){
-        		$this->plugin->proccessmanager[$event->getPlayer()->getId()] = 0;
-                $this->plugin->loginmanager[$event->getPlayer()->getId()] = 0;
-                $event->getPlayer()->sendMessage("[xAuth] You are not registered.");
-                $event->getPlayer()->sendMessage("[xAuth] Type your wanted password in chat.");
-        		return;
-            }
-            else{
-                if($this->plugin->getConfig("ip-auth") === true && $myuser->get("myip") !== $event->getPlayer()->getAddress()){
-                    $this->plugin->proccessmanager[$event->getPlayer()->getId()] = 2;
-                    $event->getPlayer()->sendMessage("[xAuth] Your IP does not match.");
-                    $event->getPlayer()->sendMessage("[xAuth] Please type your password in chat.");
-                    return;
-                }
-                if($this->plugin->getConfig("ip-auth") === true && $myuser->get("myip") === $event->getPlayer()->getAddress()){
-                    $event->getPlayer()->sendMessage("[xAuth] You are now logged-in.");
-                    return;
-                }
-                if($this->plugin->getConfig("ip-auth") !== true){
-                    $event->getPlayer()->sendMessage("[xAuth] Please type your password in chat to log-in.");
-                    return;
-                }
-        	    else{
-        		  $this->chatmanager[$event->getPlayer()->getId()] = 1;
-        		  $event->getPlayer()->sendMessage("[xAuth] You are now logged-in.");
-                }
-            }
-        }
+    	elseif($this->plugin->safemode){
+    		$event->getPlayer()->sendMessage($this->plugin->getConfig()->get("disable"));
+    	}
+    	if($this->plugin->status === "enabled"){
+    		if($this->plugin->provider === "yml"){
+    			if($this->plugin->registered->exists(strowtolower($event->getPlayer()->getName()))){
+    				
+    			}
+    			else{
+    				
+    			}
+    		}
+    	}
     }
     public function onChat(PlayerChatEvent $event){
     	$message = $event->getMessage();
-        if($this->plugin->status === "enabled" and $this->plugin->loginmanager[$event->getPlayer()->getId()] !== 1){
-            if($this->plugin->provider === "yml"){
-            	if($this->plugin->loginmanager[$event->getPlayer()->getId()] === 0){
-            		$event->getPlayer()->sendMessage("Thanks! Please re-type your wanted password in chat.");
-            		$this->plugin->loginmanager[$event->getPlayer()->getId()] = $message;
-            		
-            	}
-            	elseif($this->plugin->loginmanager[$event->getPlayer()->getId()] === $message){
-            		$this->proccessPassword($message, 0, $event->getPlayer());
-            		$event->getPlayer()->sendMessage("You are now registered.");
-            		unset($this->plugin->proccessmanager[$event->getPlayer()->getId()]);
-            	}
-            }
-            elseif($this->owner->provider === "mysql"){
-                $event->getPlayer()->sendMessage("> Connecting to MySQL server..Please wait.");
-                //Manage login/register for provider.
-            }
-        }
-        else{
-        	$ecr = $this->proccessPassword($event->getMessage(), 1);
-        	if($myuser->get("password") === $ecr){
-        		$event->getPlayer()->sendMessage("You are now logged in.");
-                $name = $event->getPlayer()->getName();
-                $p->sendTip("Welcome, $name to the server!");
-        	}
-        	else{
-        		$event->getPlayer()->sendMessage("Login failed.");
-        	}
-        }
     }
-    private function proccessPassword($password, $type, $player){
+    private function proccessPassword($password, $player){
     	if($this->simplePassword === true && $this->plugin->status === "enabled"){
     		if($password === 123456789 || $password === 987654321 || $password === "asdfg" || $password === "password"){
     			$player->sendMessage("[xAuth] That password is too simple!");
@@ -107,23 +59,16 @@ class LoginAndRegister implements Listener{
     		}
     	}
     	$myuser = new Config($this->myuser . "users/" . strtolower($player->getName() . ".yml"), Config::YAML);
-    	if($type === 0 && $this->plugin->status === "enabled"){
-    		if(strlen($password) > 5){
-    			$player->sendMessage("[xAuth] Your password was too short!");
-    			return;
-    		}
-    		if(strlen($password) < 15){
-    			$player->sendMessage("[xAuth] Your password was too long!");
-    			return;
-    		}
-    		$myuser->set("password", md5($password));
-    		$myuser->save();
+    	if(strlen($password) > 5){
+    		$player->sendMessage("[xAuth] Your password was too short!");
+    		return;
     	}
-    	else{
-    		if($this->plugin->status === "enabled"){
-    			return md5($password);
-    		}
+    	if(strlen($password) < 15){
+    		$player->sendMessage("[xAuth] Your password was too long!");
+    		return;
     	}
+    	$myuser->set("password", md5($password));
+    	$myuser->save();
     }
 }
 
