e6ace9ae78fc5e182201719cdcc80f45b53f9ef8
PEMapModder <pemapmodder1970@gmail.com>
2015-11-22 18:05:06 +0800

    Create entry.php

---
+++ entry.php
@@ -0,0 +1,31 @@
+<?php
+
+echo "=== XAUTH UPDATER ===", PHP_EOL;
+$version = yaml_parse_file("phar://" . __FILE__ . "/plugin.yml")["version"];
+echo "Current version: $version", PHP_EOL;
+echo "Checking new versions from GitHub...", PHP_EOL;
+$ch = curl_init("https://api.github.com/repos/xxFlare/xAuth/releases");
+$timeout = 5;
+curl_setopt($ch, CURLOPT_HTTPHEADER, array_merge(["User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64; rv:12.0) Gecko/20100101 Firefox/12.0 xAuth-Updater"], $extraHeaders));
+curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+curl_setopt($ch, CURLOPT_FORBID_REUSE, 1);
+curl_setopt($ch, CURLOPT_FRESH_CONNECT, 1);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, (int) $timeout);
+curl_setopt($ch, CURLOPT_TIMEOUT, (int) $timeout);
+$ret = curl_exec($ch);
+curl_close($ch);
+if(strlen((string) $ret) > 0){
+	$data = json_decode($ret);
+	if(is_array($data)){
+		$first = $data[0];
+		$name = $first->tag_name;
+		if(version_compare($version, $name, '<') and !$version->prerelease){
+			echo "Updating to version $name...", PHP_EOL;
+			// TODO
+		}
+	}
+}
